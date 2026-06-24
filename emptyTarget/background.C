297f92b78d76a069c77a6a1e10651b564ff19101
YuanLiSDU <lyuan@mail.sdu.edu.cn>
2026-06-24 12:11:02 +0800

    update

--- emptyTarget/background.C
+++ emptyTarget/background.C
@@ -127,7 +127,7 @@ static void fillHists(const std::vector<TString> &fnames,
 void background(const char *files = "../data/empty_target/prad_24386.filtered.root",
                 double lc = 1.0)
 {
-    float Ebeam = 3485.71f; // MeV
+    float Ebeam = 2239.51f; // MeV
 
     // Parse comma-separated file list
     std::vector<TString> fileList;
